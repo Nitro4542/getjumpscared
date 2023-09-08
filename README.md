6c7f539a72060b34f997d69f54a04dfdffc2f8f1
Nitro4542 <100068674+Nitro4542@users.noreply.github.com>
2023-09-08 17:38:42 +0200

    Added a lot of stuff

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # getjumpscared
- You will get jumpscared
+A single powershell script which will trigger a jumpscare, which was written in Python.
+
+## Dependencies
+
+- Python  
+
+- The following libraries, which can be installed from the terminal like this:  
+
+```bat
+pip install opencv-python
+pip install pygame
+pip install pycaw
+pip install comtypes
+```
---
+++ scripts/jumpscare.py
@@ -0,0 +1,57 @@
+# Attention: If you run this program, you will get jumpscared.
+# If you die from a heart attack I am not responsible.
+
+# Set volume to maximum
+import cv2
+import pygame
+import ctypes
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+
+volume.SetMasterVolumeLevel(0.0, None)
+
+# Play video
+# Path to the video & audio
+video_path = 'js_video.mp4'
+audio_path = 'js_audio.mp3'
+# Create a VideoCapture object
+cap = cv2.VideoCapture(video_path)
+
+# Check if the video opened successfully
+if not cap.isOpened():
+    print("Error opening the video.")
+    exit()
+
+# Create a video window and open it in fullscreen mode
+cv2.namedWindow("Video", cv2.WND_PROP_FULLSCREEN)
+cv2.setWindowProperty("Video", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+# Init pygame & play audio
+pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load(audio_path)
+pygame.mixer.music.play()
+pygame.event.wait()
+
+# Display video frames & hide cursor
+while True:
+    ctypes.windll.user32.ShowCursor(False)
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    cv2.imshow("Video", frame)
+
+    # Play the video and wait for a key to close the window
+    if cv2.waitKey(30) & 0xFF == ord('q'):
+        break
+
+# Release the VideoCapture object and close the window & restore cursor
+ctypes.windll.user32.ShowCursor(True)
+cap.release()
+cv2.destroyAllWindows()
